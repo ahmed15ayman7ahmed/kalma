ba142eafa1ccd3ee2b4185430a5c174bc566b7e0
ahmed15ayman7ahmed <151575807+ahmed15ayman7ahmed@users.noreply.github.com>
2025-01-05 13:47:20 +0200

    Add files via upload

---
+++ js/plugin.js
@@ -0,0 +1,57 @@
+$(".rush").click(function () {
+    $("html,body").animate({
+        scrollTop: $(".block4").offset().top}, "slow")
+});
+
+
+$(".form-control-input").on("focus" , function () {
+    $("#btnSubmit").removeAttr("disabled");
+});
+function mytime() {
+    $("#btnSubmit").removeAttr("disabled");
+}
+
+
+var url = $(location).attr("href"),
+    parts = url.split("="),
+    last_part = parts[parts.length - 1];
+last_part != url && $("#article").val(last_part);
+
+
+function SubmitForm() {
+    $("#btnSubmit").attr("disabled","disabled");
+    setTimeout(mytime , 4000);
+    var jsFormData = $("#contact-form").serialize();
+    $.ajax({
+        type: "POST",
+        url: "https://script.google.com/macros/s/AKfycbxVT_rfoGWRMjXv9-unMyaDZvsoXB6Rw9hQ1ZpHf4GP4UcR8uBf15BT0aBWNC44oK_Bbw/exec",
+        data: jsFormData,
+        async: false,
+        dataType: "xml",
+        success: function (data) {
+            SuccessResponse();
+        }
+    });
+    SuccessResponse();
+    window.open('thanks.html', '_self');
+};
+
+function SuccessResponse() {
+    $('#btnSubmit').attr("disabled", 'true');
+    $("#contact-form :input").attr('disabled', true);
+};
+
+function validateForm() {
+    $("#contact-form").validate();
+};
+$().ready(function () {
+    // validate the comment form when it is submitted
+    $("#contact-form").validate({
+        rules: {
+            "entry.710811246": {
+                required: true,
+                digits: true
+            }
+        }
+    });
+});
